1ac2c5d44e444302c46d2f07a309cd0ab464f858
holgerleichsenring <holger.leichsenring@outlook.com>
2018-01-19 15:48:23 +0100

    fix(Sample): Removed necessity to specify a name for the sample url to see if dependency injection works

--- FunctifiedAutofac.Sample/Services/Functions/TestFunction1.cs
+++ FunctifiedAutofac.Sample/Services/Functions/TestFunction1.cs
@@ -19,20 +19,7 @@ namespace FunctifiedAutofac.Sample.Services.Functions
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
-
-            // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
-
-            // Set name to query string or body data
-            name = name ?? data?.name;
-
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK,$"Hello {name}. Dependency injection sample returns '{testit.CallMe()}'");
+            return req.CreateResponse(HttpStatusCode.OK,$"Hello. Dependency injection sample returns '{testit.CallMe()}'");
         }
     }
 }

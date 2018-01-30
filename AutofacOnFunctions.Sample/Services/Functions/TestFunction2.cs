4f9742b58362345bfd3b68e9d2bf3cf08b5f26d5
holgerleichsenring <holger.leichsenring@outlook.com>
2018-01-30 12:08:19 +0100

    fix(Sample): Corrected assembly name and base namespace name feat(Sample): Added second test function

---
+++ AutofacOnFunctions.Sample/Services/Functions/TestFunction2.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AutofacOnFunctions.Core.Services.Ioc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace AutofacOnFunctions.Sample.Services.Functions
+{
+    public static class TestFunction2
+    {
+        [FunctionName("TestFunction2")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, 
+            TraceWriter log,
+            [Inject]  TestIt testit)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            return req.CreateResponse(HttpStatusCode.OK,$"Hello. Dependency injection sample returns '{testit.CallMe()}'");
+        }
+    }
+}
